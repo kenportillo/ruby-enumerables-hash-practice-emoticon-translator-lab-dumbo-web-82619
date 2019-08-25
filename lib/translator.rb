0e3197ed419e17b6e9942d60ef6ab7f124255ffe
Kenneth Portillo <portillokenneth@gmail.com>
2019-08-25 01:02:29 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,25 +1,3 @@
-# # require modules here
-# require "pry"
-# require "yaml"
-
-# def load_library (array)
-#   emoticons = yaml.load_file(array)
-#   emoticon_hash = hash.new 
-
-#   emoticon_hash["get_emoticon"] = hash.new 
-#   emoticon_hash["get_meaning"] = hash.new 
-#   # code goes here
-  
-# end
-
-# def get_japanese_emoticon
-#   # code goes here
-# end
-
-# def get_english_meaning
-#   # code goes here
-# end
-
 require 'yaml'
 require 'pry'
 
